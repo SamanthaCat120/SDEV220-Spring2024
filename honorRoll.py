42372a9f1bcf3b5b817dcd1fe723ddb0c59b4002
SamanthaCat120 <53274929+SamanthaCat120@users.noreply.github.com>
2024-01-26 17:09:08 -0500

    Add honorRoll.py

---
+++ honorRoll.py
@@ -0,0 +1,23 @@
+def honorRoll():
+    while True:
+        lname = input("Enter student's last name (or type 'ZZZ' to quit): ")
+
+        #checks to see if user wants to quit
+        if lname == "ZZZ":
+            break
+
+        fname = input("Enter student's first name: ")
+
+        gpa = float(input("Enter student's GPA: "))
+       
+        if gpa >= 3.5:
+           print(f"Congratulations, {fname} {lname} made the Dean's List!")
+
+        elif 3.25 < gpa < 3.5:
+            print(f"Congratulations, {fname} {lname} made the Honor Roll!")
+
+        else:
+            print(f"Keep working!{fname} {lname} You're almost there!")
+
+if __name__ == "__main__":
+    honorRoll()
