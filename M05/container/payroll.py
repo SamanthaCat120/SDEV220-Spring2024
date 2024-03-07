580da85887608e91548f7ecfdeef2ae93e85d81a
SamanthaCat120 <53274929+SamanthaCat120@users.noreply.github.com>
2024-03-07 17:26:02 -0500

    Add emoji to payroll

--- M05/container/payroll.py
+++ M05/container/payroll.py
@@ -9,6 +9,7 @@ Last updated on Sat Feb 24 19:56:46 2024
 
 import datetime
 from functools import wraps
+import emoji
 
 TAX_RATE = 0.14
 OVERTIME_THRESHOLD = 40
@@ -27,7 +28,8 @@ def getInputs():
     name = input("WHAT is your name? ")
     hours = float(input("How many hours did you work? "))
     rate = float(input("What is your hourly rate? "))
-    return name, hours, rate
+    need = float(input("How much did you need this week? "))
+    return name, hours, rate, need
 
 @logger
 def calculateGross(hours, rate):
@@ -51,8 +53,14 @@ def printCheck(net, name):
         toFile.write("Amount: $ \t" + format(net, ".2f"))
 
 if __name__=="__main__":
-    name, hours, rate = getInputs()
+    name, hours, rate, need = getInputs()
     print(name, hours, rate)
     gross = calculateGross(hours, rate)
     net = deductTaxes(gross)
     print('Your net amount is $ ', format(net, ".2f"))
+    if need <= net:
+        print("Yay, you made what you need to spend this week." + emoji.emojize(":grinning_face_with_big_eyes:"))
+
+    else:
+        print(emoji.emojize(":disappointed_face:") + "I guess you'll have to work more next week.")
+
