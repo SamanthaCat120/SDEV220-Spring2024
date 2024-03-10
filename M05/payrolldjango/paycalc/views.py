51c952174a2f1f6012daab2ea702b35db461c370
SamanthaCat120 <53274929+SamanthaCat120@users.noreply.github.com>
2024-03-10 15:19:18 -0400

    update payroll project

--- M05/payrolldjango/paycalc/views.py
+++ M05/payrolldjango/paycalc/views.py
@@ -7,7 +7,6 @@ from django.http import HttpResponse
 def homePage(request):
 	return render(request, "homePage.html")
 
-
 def simple(request):
 	data = {"title": "Simple Template", "message": "Hello World"}
 	return render(request, "simple.html", data)
@@ -17,6 +16,19 @@ def inputs(request):
 
 def calculatePay(request):
 	return render(request, "payroll.html")
+
+def calculateGross(request):
+	data = {"name": "name", "rate": "rate", "hours": "hours", "need": "need"}
+	if hours > OVERTIME_THRESHOLD:
+		gross = (hours - OVERTIME_THRESHOLD) * rate * OVERTIME_MULTIPLIER + OVERTIME_THRESHOLD * rate
+	else:
+		gross = hours * rate
+		return gross
+
+def deductTaxes(gross):
+	deduction = gross * TAX_RATE
+	net = gross - deduction
+	return net
 
 
 
